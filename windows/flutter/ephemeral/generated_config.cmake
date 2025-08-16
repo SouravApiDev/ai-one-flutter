0dc50f6c0d844b771fd7a4759760799923c08e1a
Sourav <38432685+SouravApiDev@users.noreply.github.com>
2025-08-17 01:05:29 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter_windows_3.32.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter\\aione" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter_windows_3.32.5-stable\\flutter"
+  "PROJECT_DIR=D:\\flutter\\aione"
+  "FLUTTER_ROOT=E:\\flutter_windows_3.32.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter\\aione\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter\\aione"
+  "FLUTTER_TARGET=D:\\flutter\\aione\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzIuOA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZWRhZGE3YzU2ZQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZWYwY2QwMDA5MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter\\aione\\.dart_tool\\package_config.json"
+)
